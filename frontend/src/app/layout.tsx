2bb1a4f117ac1101dcf70c70272a1a9f4516eb38
ajmal-stack <ajmal69ktr@gmail.com>
2025-05-14 10:35:12 +0530

    issue solved: tailwind css and global css

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { CartProvider } from './context/CartContext';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const geistSans = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'TechShop - Electronics Store',
@@ -25,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.className} antialiased`}>
         <CartProvider>{children}</CartProvider>
       </body>
     </html>
